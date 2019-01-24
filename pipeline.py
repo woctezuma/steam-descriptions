c08314dcbf77a4a32a100041805dc7756fbb9d4f
Wok <woctezuma@users.noreply.github.com>
2019-01-24 21:58:04 +0100

    WIP Doc2Vec

--- pipeline.py
+++ pipeline.py
@@ -1,14 +1,33 @@
-from gensim.models import Word2Vec
+from time import time
 
-from utils import get_word_model_file_name, load_game_names, load_tokens
+import gensim
+
+from utils import load_tokens, load_game_names
+
+
+def read_corpus(steam_tokens):
+    for app_id, tokens in steam_tokens.items():
+        yield gensim.models.doc2vec.TaggedDocument(tokens, [int(app_id)])
 
 
 def main():
+    game_names = load_game_names()
+
     steam_tokens = load_tokens()
 
-    model = Word2Vec.load(get_word_model_file_name())
+    corpus = list(read_corpus(steam_tokens))
 
-    game_names = load_game_names()
+    model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=40)
+
+    model.build_vocab(corpus)
+
+    start = time()
+    model.train(corpus, total_examples=model.corpus_count, epochs=model.epochs)
+    print('Cell took %.2f seconds to run.' % (time() - start))
+
+    doc_id = '583950'  # Artifact
+    inferred_vector = model.infer_vector(steam_tokens[doc_id])
+    sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
 
     return True
 
