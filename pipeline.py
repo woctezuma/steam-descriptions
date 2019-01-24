860118a99fbfa1848f538f2032ac953db50a9285
Wok <woctezuma@users.noreply.github.com>
2019-01-25 00:22:58 +0100

    Doc2Vec is running as intended

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,9 @@
+import multiprocessing
 from time import time
 
 import gensim
 
+from sentence_models import print_most_similar_sentences
 from utils import load_tokens, load_game_names
 
 
@@ -11,23 +13,44 @@ def read_corpus(steam_tokens):
 
 
 def main():
+    num_workers = multiprocessing.cpu_count()
+
     game_names = load_game_names()
 
     steam_tokens = load_tokens()
 
     corpus = list(read_corpus(steam_tokens))
 
-    model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=40)
+    doc_model_file_name = 'data/doc2vec.model'
+    load_model = True
+    train_model = False
 
-    model.build_vocab(corpus)
+    if load_model:
+        model = gensim.models.doc2vec.Doc2Vec.load(doc_model_file_name)
 
-    start = time()
-    model.train(corpus, total_examples=model.corpus_count, epochs=model.epochs)
-    print('Cell took %.2f seconds to run.' % (time() - start))
+    else:
+        model = gensim.models.doc2vec.Doc2Vec(workers=num_workers, min_count=2, epochs=40)
+
+        model.build_vocab(corpus)
+
+    if train_model:
+        start = time()
+        model.train(corpus, total_examples=model.corpus_count, epochs=model.epochs)
+        print('Cell took %.2f seconds to run.' % (time() - start))
+
+        model.save(doc_model_file_name)
 
     doc_id = '583950'  # Artifact
     inferred_vector = model.infer_vector(steam_tokens[doc_id])
     sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
+
+    similarity_scores = dict()
+    for app_id, similarity_value in sims:
+        # Remove dummy appIDs
+        if str(app_id) in game_names:
+            similarity_scores[str(app_id)] = similarity_value
+
+    print_most_similar_sentences(similarity_scores)
 
     return True
 
