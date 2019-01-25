152332c1d16e74fab0fbeca8c9a3ae903afe6fd3
Wok <woctezuma@users.noreply.github.com>
2019-01-25 14:37:46 +0100

    Display the top 3 most similar documents

--- pipeline.py
+++ pipeline.py
@@ -23,7 +23,8 @@ def main(train_from_scratch=False, enforce_training=False):
     for query_app_id in ['10', '620', '105600', '264710', '292030', '294100', '364470', '504230', '519860', '531640',
                          '560130', '582010', '583950', '588650', '590380', '620980', '638970', '644560', '646570',
                          '653530', '683320', '698780', '731490', '742120', '812140', '863550', '973760']:
-        compute_similarity_using_doc2vec_model(query_app_id, steam_tokens, model, avoid_inference=True)
+        compute_similarity_using_doc2vec_model(query_app_id, steam_tokens, model, avoid_inference=True,
+                                               num_items_displayed=3)
 
     # Check the relevance of the corresponding word2vec
     for query_word in ['anime', 'fun', 'violent']:
