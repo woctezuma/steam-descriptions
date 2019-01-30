cb62e621bf1c9a051d2e88cf53636531fc4bcb1c
Wok <woctezuma@users.noreply.github.com>
2019-01-30 16:24:41 +0100

    Reformat with a named tuple

--- SIF_embedding.py
+++ SIF_embedding.py
@@ -1,5 +1,7 @@
 # Reference: https://github.com/PrincetonML/SIF
 
+from collections import namedtuple
+
 import numpy as np
 from sklearn.decomposition import TruncatedSVD
 
@@ -59,3 +61,31 @@ def sif_embedding(word_embed, x, w, params):
     if params.rmpc > 0:
         emb = remove_pc(emb, params.rmpc)
     return emb
+
+
+if __name__ == '__main__':
+    import data_io
+
+    # input
+    word_file = '../data/glove.840B.300d.txt'  # word vector file, can be downloaded from GloVe website
+    weight_file = '../auxiliary_data/enwiki_vocab_min200.txt'  # each line is a word and its frequency
+    weight_parameter = 1e-3  # the parameter in the SIF weighting scheme, usually in the range [3e-5, 3e-3]
+    sentences = ['this is an example sentence', 'this is another sentence that is slightly longer']
+
+    # load word vectors
+    (words, We) = data_io.get_word_map(word_file)
+    # load word weights
+    word2weight = data_io.get_word_weight(weight_file,
+                                          weight_parameter)  # word2weight['str'] is the weight for the word 'str'
+    weight4ind = data_io.get_weight(words, word2weight)  # weight4ind[i] is the weight for the i-th word
+    # load sentences
+    x, m, _ = data_io.sentences2idx(sentences,
+                                    words)  # x is the array of word indices,
+    # m is the binary mask indicating whether there is a word in that location
+    w = data_io.seq2weight(x, m, weight4ind)  # get word weights
+
+    # set parameters
+    Params = namedtuple('Params', 'rmpc')
+    params = Params(1)  # number of principal components to remove in SIF weighting scheme
+    # get SIF embedding
+    embedding = sif_embedding(We, x, w, params)  # embedding[i,:] is the embedding for sentence i
