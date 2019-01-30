b6d508e970427e2408e770ad0118391b3cca734d
Wok <woctezuma@users.noreply.github.com>
2019-01-30 19:38:36 +0100

    Add arguments: - whether to compute from scratch or load X from the disk, - whether to normalize word vectors.

--- sif_embedding_perso.py
+++ sif_embedding_perso.py
@@ -11,7 +11,7 @@ from sentence_models import filter_out_words_not_in_vocabulary
 from utils import load_tokens, load_game_names
 
 
-def main():
+def main(compute_from_scratch=True, use_unit_vectors=False):
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
     game_names, _ = load_game_names(include_genres=False, include_categories=False)
@@ -20,96 +20,107 @@ def main():
 
     documents = list(steam_tokens.values())
 
-    dct = Dictionary(documents)
-    print(len(dct))
-    dct.filter_extremes(no_below=5, no_above=0.5)  # TODO choose parameters
-    print(len(dct))
+    if compute_from_scratch:
 
-    model = Word2Vec(documents)
+        dct = Dictionary(documents)
+        print(len(dct))
+        dct.filter_extremes(no_below=5, no_above=0.5)  # TODO choose parameters
+        print(len(dct))
 
-    wv = model.wv
+        model = Word2Vec(documents)
 
-    wv.init_sims(replace=True)  # TODO IMPORTANT choose whether to normalize vectors
+        wv = model.wv
 
-    index2word_set = set(wv.index2word)
+        if use_unit_vectors:
+            wv.init_sims(replace=True)  # TODO IMPORTANT choose whether to normalize vectors
 
-    num_games = len(steam_tokens)
+        index2word_set = set(wv.index2word)
 
-    word_counter = {}
+        num_games = len(steam_tokens)
 
-    counter = 0
-    for app_id in steam_tokens:
-        counter += 1
+        word_counter = {}
 
-        if (counter % 1000) == 0:
-            print('[{}/{}] appID = {} ({})'.format(counter, num_games, app_id, game_names[app_id]))
+        counter = 0
+        for app_id in steam_tokens:
+            counter += 1
 
-        reference_sentence = steam_tokens[app_id]
-        reference_sentence = filter_out_words_not_in_vocabulary(reference_sentence, index2word_set)
+            if (counter % 1000) == 0:
+                print('[{}/{}] appID = {} ({})'.format(counter, num_games, app_id, game_names[app_id]))
 
-        for word in reference_sentence:
-            if word in wv.vocab:
-                try:
-                    word_counter[word] += 1
-                except KeyError:
-                    word_counter[word] = 1
+            reference_sentence = steam_tokens[app_id]
+            reference_sentence = filter_out_words_not_in_vocabulary(reference_sentence, index2word_set)
 
-    total_counter = sum(word_counter.values())
+            for word in reference_sentence:
+                if word in wv.vocab:
+                    try:
+                        word_counter[word] += 1
+                    except KeyError:
+                        word_counter[word] = 1
 
-    word_frequency = dict()
-    for word in word_counter:
-        word_frequency[word] = word_counter[word] / total_counter
+        total_counter = sum(word_counter.values())
 
-    sentence_vector = {}
-    X = np.zeros([num_games, wv.vector_size])
-    alpha = 1e-3
+        word_frequency = dict()
+        for word in word_counter:
+            word_frequency[word] = word_counter[word] / total_counter
 
-    counter = 0
-    for (i, app_id) in enumerate(steam_tokens.keys()):
-        counter += 1
+        sentence_vector = {}
+        X = np.zeros([num_games, wv.vector_size])
+        alpha = 1e-3
 
-        if (counter % 1000) == 0:
-            print('[{}/{}] appID = {} ({})'.format(counter, num_games, app_id, game_names[app_id]))
+        counter = 0
+        for (i, app_id) in enumerate(steam_tokens.keys()):
+            counter += 1
 
-        reference_sentence = steam_tokens[app_id]
-        reference_sentence = filter_out_words_not_in_vocabulary(reference_sentence, index2word_set)
+            if (counter % 1000) == 0:
+                print('[{}/{}] appID = {} ({})'.format(counter, num_games, app_id, game_names[app_id]))
 
-        weighted_vector = np.zeros(wv.vector_size)
+            reference_sentence = steam_tokens[app_id]
+            reference_sentence = filter_out_words_not_in_vocabulary(reference_sentence, index2word_set)
 
-        for word in reference_sentence:
-            weight = (alpha / (alpha + word_frequency[word]))
+            weighted_vector = np.zeros(wv.vector_size)
 
-            # TODO IMPORTANT Why use the normalized word vectors instead of the raw word vectors?
-            weighted_vector += weight * wv.vectors_norm[wv.vocab[word].index]
+            for word in reference_sentence:
+                weight = (alpha / (alpha + word_frequency[word]))
 
-        sentence_vector[app_id] = weighted_vector / len(reference_sentence)
-        X[i, :] = sentence_vector[app_id]
+                # TODO IMPORTANT Why use the normalized word vectors instead of the raw word vectors?
+                if use_unit_vectors:
+                    weighted_vector += weight * wv.vectors_norm[wv.vocab[word].index]
+                else:
+                    weighted_vector += weight * wv.vectors[wv.vocab[word].index]
 
-    # Reference: https://stackoverflow.com/a/11620982
-    X = np.where(np.isfinite(X), X, 0)
+            sentence_vector[app_id] = weighted_vector / len(reference_sentence)
+            X[i, :] = sentence_vector[app_id]
 
-    X = remove_pc(X)
+        # Reference: https://stackoverflow.com/a/11620982
+        X = np.where(np.isfinite(X), X, 0)
 
-    np.save('data/X.npy', X)
+        X = remove_pc(X)
 
-    query_app_ids = ['620', '364470', '504230', '583950', '646570', '863550', '794600']
+        np.save('data/X.npy', X)
+
+    else:
+        X = np.load('data/X.npy', mmap_mode='r')
 
     app_ids = list(steam_tokens.keys())
 
+    query_app_ids = ['620', '364470', '504230', '583950', '646570', '863550', '794600']
+    query_indices = [app_ids.index(query_app_id) for query_app_id in query_app_ids]
+
     start = time()
-    sim = cosine_similarity(X)
+    sim = cosine_similarity(X[query_indices, :], X)
     print('Elapsed time: {%.2f}' % (time() - start))
 
-    for query_app_id in query_app_ids:
-        query_index = app_ids.index(query_app_id)
-        v = sim[query_index, :]
+    for (i, query_app_id) in enumerate(query_app_ids):
+        print('\nQuery appID: {} ({})'.format(query_app_id, game_names[query_app_id]))
+        v = sim[i, :]
 
         # Reference: https://stackoverflow.com/a/23734295
         ind = np.argpartition(v, -10)[-10:]
-        sorted_ind = ind[np.argsort(v[ind])]
+        sorted_ind = reversed(ind[np.argsort(v[ind])])
 
-        for i in sorted_ind:
-            print(app_ids[i])
+        for j in sorted_ind:
+            app_id = app_ids[j]
+            print('{:2}) similarity: {:.1%} ; appID: {} ({})'.format(j, v[j], app_id, game_names[app_id]))
 
     return
 
