8d9d7e414341934d32ddd74eb08901a60a1cb7f0
Wok <woctezuma@users.noreply.github.com>
2019-05-25 10:38:52 +0200

    Print banners for unique games in BBCode format (to post on forums)

--- find_unique_games.py
+++ find_unique_games.py
@@ -83,10 +83,21 @@ def load_sim_dict():
     return sim_dict
 
 
+def get_small_banner_url(app_id):
+    small_banner_url = 'https://steamcdn-a.akamaihd.net/steam/apps/' + str(app_id) + '/capsule_sm_120.jpg'
+    return small_banner_url
+
+
+def get_bb_code_linked_image(app_id):
+    bb_code_linked_image = '[URL={}][IMG]{}[/IMG][/URL]'.format(get_store_url(app_id), get_small_banner_url(app_id))
+    return bb_code_linked_image
+
+
 def print_unique_games(sim_dict,
                        similarity_threshold,
                        game_names,
-                       only_print_banners=False):
+                       only_print_banners=False,
+                       use_markdown=True):
     # Markdown
     # Reference: https://stackoverflow.com/a/14747656
     image_link_str = '[<img alt="{}" src="{}" width="{}">]({})'
@@ -103,8 +114,13 @@ def print_unique_games(sim_dict,
 
             app_name = get_app_name(app_id, game_names=game_names)
             if only_print_banners:
-                # Markdown
-                print(image_link_str.format(app_name, get_banner_url(app_id), image_width, get_store_url(app_id)))
+                if use_markdown:
+                    # Markdown
+                    print(image_link_str.format(app_name, get_banner_url(app_id), image_width, get_store_url(app_id)))
+                else:
+                    # BBCode
+                    end_of_entry = ' '  # Either a line break '\n' or a space ' '. Prefer spaces if you post to a forum.
+                    print(get_bb_code_linked_image(app_id), end=end_of_entry)
             else:
                 print('{}) similarity = {:.2f} ; appID = {} ({})'.format(counter + 1,
                                                                          similarity_value,
@@ -117,7 +133,8 @@ def print_unique_games(sim_dict,
 def main(num_items_displayed=2,
          similarity_threshold=0.149,
          update_sim_dict=False,
-         only_print_banners=False):
+         only_print_banners=False,
+         use_markdown=True):
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
     game_names, steam_tokens, app_ids = load_input()
@@ -142,7 +159,8 @@ def main(num_items_displayed=2,
     unique_app_ids = print_unique_games(sim_dict,
                                         similarity_threshold,
                                         game_names,
-                                        only_print_banners)
+                                        only_print_banners=only_print_banners,
+                                        use_markdown=use_markdown)
 
     return
 
